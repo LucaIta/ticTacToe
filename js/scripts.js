973eac82bdd0268cfebf2c87087f6b263edbc12f
c.g. Acharjya and Luca Quatela <c.g.@>
2016-04-06 11:25:26 -0700

    initial logic and file structure

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// function Players (mark) {
+//   this.mark = mark;
+// };
+//
+// Players.prototype.selectedAreas {
+//   var x = ;
+//
+// };
+
+$(document).ready(function(){
+  var newarrayX = [];
+
+
+  var playerX = function(){
+     $(".col-md-2").click(function() {
+       var x_locations =  $(this).index()
+       newarray.push(x_locations);
+
+     })
+
+// insert string into div and make new array of the div id locations
+// do the same for playerO
+
+
+
+
+})
+
+
+player.mark(2,2)
+
+var winArray =[[a,b,c], [d,e,f], [g,h,l], [a,d,g], [b,e,h], [c,f,l], [a,e,l], [c,e,g]]
+
+var winnerX
+Array.prototype.contains = function(obj) {
+  // where playerx exists in any of the full array values for winArray
+  return true
+  alert("O is the winner")}
+
+var winnerO =
+Array.prototype.contains = function(obj) {
+  alert("O is the winner")
+}
