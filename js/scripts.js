63f473c3ae781acade1dbf6fab5dfe750c3a200c
c.g. Acharjya and Luca Quatela <c.g.@>
2016-04-06 15:45:35 -0700

    add file with the logic needed to check if the combination selected is a winning one

--- js/scripts.js
+++ js/scripts.js
@@ -11,33 +11,45 @@ $(document).ready(function(){
   var newarrayX = [];
 
 
-  var playerX = function(){
-     $(".col-md-2").click(function() {
-       var x_locations =  $(this).index()
-       newarray.push(x_locations);
 
-     })
+  $(".col-md-2").click(function() {
+    var x_locations = $(this).attr("id");
+    newarrayX.push(x_locations);
+    console.log(newarrayX);
+  })
+});
 
+ //
+ //  var playerX = function(){
+ //     $(".col-md-2").click(function() {
+ //       var x_locations =  $(this).index()
+ //       newarray.push(x_locations);
+ //      });
+ //   }
+ // })
 // insert string into div and make new array of the div id locations
 // do the same for playerO
 
 
 
 
-})
+// })
+
+//
+  // var winArrayX =[[a,b,c], [d,e,f], [g,h,l], [a,d,g],
+  //             [b,e,h], [c,f,l], [a,e,l], [c,e,g]]
 
 
-player.mark(2,2)
+            // }
 
-var winArray =[[a,b,c], [d,e,f], [g,h,l], [a,d,g], [b,e,h], [c,f,l], [a,e,l], [c,e,g]]
-
-var winnerX
-Array.prototype.contains = function(obj) {
-  // where playerx exists in any of the full array values for winArray
-  return true
-  alert("O is the winner")}
-
-var winnerO =
-Array.prototype.contains = function(obj) {
-  alert("O is the winner")
-}
+//
+// var winnerX
+// Array.prototype.contains = function(obj) {
+//   // where playerx exists in any of the full array values for winArray
+//   return true
+//   alert("O is the winner")}
+//
+// var winnerO =
+// Array.prototype.contains = function(obj) {
+//   alert("O is the winner");
+// };
