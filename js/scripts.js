15834702efe47b9f7ac5bef4796948b1ced67b2b
C.g. Acharjya and Luca Quatela <c.g.+luca@>
2016-04-07 08:52:06 -0700

    add the checking of the winning user anddisplay Xs and Os in the table

--- js/scripts.js
+++ js/scripts.js
@@ -2,64 +2,46 @@ var userX =[];
 var user0 =[];
 var clicks = 1;
 
+var winnerChecker = function (userArray){
+  var win = 0;
+  var arrayOfWinningArrays = [["a","b","c"], ["d","e","f"], ["g","h","l"], ["a","d","g"],["b","e","h"], ["c","f","l"], ["a","e","l"], ["c","e","g"]];
+  arrayOfWinningArrays.forEach(function(winningArray){
+  win = 0;
+  winningArray.forEach(function(elementOfWinningArray){
+    userArray.forEach(function(elementOFUSerArray){
+      if (elementOFUSerArray === elementOfWinningArray){
+         win ++;
+         } else {
+         }
+      })
+      if (win === 3){
+        alert ("you win");
+      }
+    })
+  })
+}
+
 $(document).ready(function(){
-  $(".col-md-2").click(function() {
+  $(".col-md-2").one("click", function() {
     clicks ++;
     if (clicks < 11){
       if (clicks % 2 === 0) {
         console.log("user x turn");
         var x_locations = $(this).attr("id");
-        // $(this).show();
-        // $("#showX").click(function(){
-        //    $("#showX").show();
-        // })
-        // $(this).show();
+        $(this).text("X");
         userX.push(x_locations);
+        winnerChecker(userX);
         console.log(userX);
       }
       else {
         console.log("user 0 turn");
         var x_locations = $(this).attr("id");
-        // $(this).show();
+        $(this).text("O");
         user0.push(x_locations);
+        winnerChecker(user0);
         console.log(user0);
       }
+
     }
   })
 })
-
-
- //
- //  var playerX = function(){
- //     $(".col-md-2").click(function() {
- //       var x_locations =  $(this).index()
- //       newarray.push(x_locations);
- //      });
- //   }
- // })
-// insert string into div and make new array of the div id locations
-// do the same for playerO
-
-
-
-
-// })
-
-//
-  // var winArrayX =[[a,b,c], [d,e,f], [g,h,l], [a,d,g],
-  //             [b,e,h], [c,f,l], [a,e,l], [c,e,g]]
-
-
-            // }
-
-//
-// var winnerX
-// Array.prototype.contains = function(obj) {
-//   // where playerx exists in any of the full array values for winArray
-//   return true
-//   alert("O is the winner")}
-//
-// var winnerO =
-// Array.prototype.contains = function(obj) {
-//   alert("O is the winner");
-// };
